56f107862a9f32f31d5924b7b69363fca6f20847
JFL110 <typus24@hotmail.com>
2020-08-23 16:40:54 +0100

    Add brush drawing on mini canvas

--- js/features/cv/miniCanvas.js
+++ js/features/cv/miniCanvas.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {  useState, useRef } from 'react'
 import CanvasDraw from "react-canvas-draw";
+import VisibilitySensor from "react-visibility-sensor";
+import brushLines from './brushSvg'
+const scaleDown = 3;
 
-export default () =>
-    <div className="mini-canvas-container">
-        <CanvasDraw
-            canvasWidth={1000}
-            canvasHeight={800}
-        />
-    </div>
+export default () => {
+    const [drawingRendered, setDrawingRendered] = useState(false);
+    const canvasRef = useRef();
+    const onVisibilityChange = visible => {
+        if (visible && !drawingRendered) {
+            setTimeout(() => {
+                canvasRef.current?.loadSaveData(JSON.stringify({
+                    lines: brushLines(),
+                    width: 1200 * scaleDown,
+                    height: 800 * scaleDown
+                }));
+            }, 500);
+            setDrawingRendered(true);
+        }
+    };
+
+    return <VisibilitySensor partialVisibility onChange={onVisibilityChange}>
+        <div className="mini-canvas-container">
+            <CanvasDraw
+                canvasWidth={1000}
+                canvasHeight={800}
+                ref={canvasRef}
+                loadTimeOffset={1}
+            />
+        </div>
+    </VisibilitySensor>
+}
