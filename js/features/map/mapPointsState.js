26a0de6e933bb72016e03551f1d162ed4c033464
JFL110 <typus24@hotmail.com>
2022-11-15 22:29:20 +0000

    New backend

--- js/features/map/mapPointsState.js
+++ js/features/map/mapPointsState.js
@@ -1,61 +1,16 @@
 import { createNetOpState, createEndpoint } from 'repileux'
-import mergeByKey from 'array-merge-by-key'
-const readPoints = response => {
 
-    if (!response?.points || !Array.isArray(response.points)) {
-        console.error("Invalid points ", response)
-        return;
-    }
-
-    const points = response.points.map(i => ({
-        id: i.l + "-" + i.g,
-        lat: i.l,
-        long: i.g,
-        isPhoto: false,
-        isMostRecent: false
-    }));
-
-    if (response.mostRecentPoint) {
-        points.push({
-            id: "r-" + response.mostRecentPoint.l + "-" + response.mostRecentPoint.g,
-            lat: response.mostRecentPoint.l,
-            long: response.mostRecentPoint.g,
-            isPhoto: false,
-            time: response.mostRecentPointTime,
-            isMostRecent: true
-        });
-    }
-
-    if (response.photos && Array.isArray(response.photos)) {
-        var photoPointId = 0;
-        let photoPoints = response.photos
-            .sort((a, b) => b.time - a.time)
-            .map(i => ({
-                id: i.url,
-                lat: i.point.l,
-                long: i.point.g,
-                time: i.time,
-                url: i.url,
-                isPhoto: true,
-                photoPointId: photoPointId++,
-                isMostRecent: false
-            }));
-        points.push(...photoPoints);
-    }
-
-    return mergeByKey('id', points);
-}
-
-export default createNetOpState({
+const mapPointsState = createNetOpState({
     name: 'points',
     endpoint: createEndpoint({
-        uri: "https://jfl110-my-location.s3.eu-west-2.amazonaws.com/live.my-location-points.json",
+        uri: "https://jamesleach-dev-location.s3.eu-west-2.amazonaws.com/location-squares.json",
     }),
     persistInLocalStorage: true,
-    persistenceVersion : 6,
+    persistenceVersion: 6,
     persistenceExpirationMs: 60 * 60 * 1000,
-    responseBodyTransformations: readPoints,
     retryDelayMs: 500,
     maxRetryCount: 4,
     isIdempotent: true,
 })
+
+export default mapPointsState;
--- js/features/map/mapTopBar.js
+++ js/features/map/mapTopBar.js
@@ -1,37 +1,35 @@
 import React from 'react'
-import { connectWithSlice, connectToOpState } from 'repileux'
+import { connectToOpState } from 'repileux'
 import timeSinceString from '../timeSinceString'
-import mapSlice from './mapSlice'
 import mapPointsState from './mapPointsState'
+
+const topBarText = points => {
+    if (points.wasError()) {
+        return ":( error - try refreshing)"
+    }
+
+    if (points.isInProgress()) {
+        return "Loading..."
+    }
+
+    if(!points.value?.updatedTime){
+        return "Updated some time ago"
+    }
+ 
+    return `Updated ${timeSinceString(new Date(points.value.updatedTime))} ago`;
+}
 
 const TopBarComponent = ({
     points,
-    setNewCentre,
-}) => {
-    const pointsLoaded = !points.isInProgress();
-    const hasFetchError = points.wasError() || (pointsLoaded && points.value?.length == 0);
-    const _mostRecentPoint = points.value?.find(p => p.isMostRecent);
-
-    const centreMap = () => setNewCentre({ lat: _mostRecentPoint.lat, lng: _mostRecentPoint.long, default: false })
-
-    const summary = () => <div onClick={centreMap} className="clickToCentreButton">
-        <h2 className="top-bar-text">
-            {"Updated " + (_mostRecentPoint ? (timeSinceString(new Date(_mostRecentPoint.time)) + " ago") : "Loading...")}
-        </h2>
-    </div>;
-
-    return <div className="map-top-bar">
+}) => <div className="map-top-bar">
         <div>
             <div className="top-bar-left">
                 <h2 className="top-bar-text noselect">Where are they?</h2>
             </div>
             <div className="top-bar-right">
-                <h2 className="top-bar-text noselect">{hasFetchError ? ":( error - try refreshing)" : (pointsLoaded ? summary() : "Loading...")}</h2>
+                <h2 className="top-bar-text noselect">{topBarText(points)}</h2>
             </div>
         </div>
     </div>;
-}
 
-export default connectToOpState(
-    mapPointsState
-    , connectWithSlice(mapSlice, TopBarComponent));
+export default connectToOpState(mapPointsState, TopBarComponent);
