cd7d47693905e61c7ec3f1f45a4f101a4d216cd0
JamesL <typus24@hotmail.com>
2025-02-09 15:30:12 +0000

    add global polyfill

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,9 @@ import cvPage from './features/cv/mainPage'
 import camperPage from './features/camper/camperPage'
 import mlDigitPage from './features/ml-digit/mlDigitPage'
 import mapModule from './features/map/mapModule'
-
 import './styles.scss'
+
+window.global = window;
 
 createApp({
   modules: [mapModule],
